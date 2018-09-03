e8708a5987ee2005345dbf35d3faac21b160fbe6
Julian McNeill <julesdowork@gmail.com>
2018-09-03 12:15:53 -0400

    DotProduct() Vector Operator

--- Battle_Tanks/Source/Battle_Tanks/Private/TankMovementComponent.cpp
+++ Battle_Tanks/Source/Battle_Tanks/Private/TankMovementComponent.cpp
@@ -32,6 +32,11 @@ void UTankMovementComponent::IntendTurnRight(float Throw)
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
 	// No need to call Super as we're replacing the functionality
-	auto Name = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s move velocity: %s"), *Name, *MoveVelocity.ToString());
+
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+	IntendMoveForward(ForwardThrow);
+	
+	//UE_LOG(LogTemp, Warning, TEXT("%s move velocity: %s"), *Name, *MoveVelocityString);
 }
